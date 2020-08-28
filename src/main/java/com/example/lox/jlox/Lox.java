0a57a28db571e2701ecf4aa25641264a089c9fc1
Harrison Chikwe <nhcdeveloper@gmail.com>
2020-08-28 22:07:35 +0100

    Initial commit.

---
+++ src/main/java/com/example/lox/jlox/Lox.java
@@ -0,0 +1,10 @@
+package com.example.lox.jlox;
+
+/**
+ * Hello world!
+ */
+public class Lox {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/example/lox/jlox/LoxTest.java
@@ -0,0 +1,18 @@
+package com.example.lox.jlox;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class LoxTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
