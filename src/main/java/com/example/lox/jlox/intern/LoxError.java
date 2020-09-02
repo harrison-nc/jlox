f16530a475364ef6aa0daeda75cda2a6693e6585
Harrison Chikwe <nhcdeveloper@gmail.com>
2020-09-02 16:24:41 +0100

    Updates LoxError adding 'where' parameter to error messages

--- src/main/java/com/example/lox/jlox/intern/LoxError.java
+++ src/main/java/com/example/lox/jlox/intern/LoxError.java
@@ -1,7 +1,11 @@
 package com.example.lox.jlox.intern;
 
+import com.example.lox.jlox.scanner.Token;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.example.lox.jlox.scanner.TokenType.EOF;
 
 public class LoxError {
     private static final List<Error> errors = new ArrayList<>();
@@ -11,35 +15,52 @@ public class LoxError {
     }
 
     public static void error(int line, String message) {
-        errors.add(Error.of(line, message));
+        errors.add(Error.of(line, "", message));
+    }
+
+    public static void error(int line, String where, String message) {
+        errors.add(Error.of(line, where, message));
+    }
+
+    public static void error(Token token, String message) {
+        if (token.type() == EOF) {
+            error(token.line(), "at end", message);
+        } else {
+            error(token.line(), "at '" + token.lexeme() + "'", message);
+        }
     }
 
     public static void report() {
         for (Error error : errors) {
             System.out.println(error);
         }
+    }
+
+    public void clear() {
         errors.clear();
     }
 
     private LoxError() {
     }
-}
 
-class Error {
-    private final int line;
-    private final String message;
+    static class Error {
+        private final int line;
+        private final String where;
+        private final String message;
 
-    private Error(int line, String message) {
-        this.line = line;
-        this.message = message;
-    }
+        private Error(int line, String where, String message) {
+            this.line = line;
+            this.message = message;
+            this.where = where;
+        }
 
-    static Error of(int line, String message) {
-        return new Error(line, message);
-    }
+        static Error of(int line, String where, String message) {
+            return new Error(line, where, message);
+        }
 
-    @Override
-    public String toString() {
-        return "[line " + line + "] Error : " + message;
+        @Override
+        public String toString() {
+            return "[line " + line + "] Error" + where + " :" + message;
+        }
     }
 }
