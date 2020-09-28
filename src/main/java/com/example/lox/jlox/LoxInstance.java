cab24ccfe3028c28f7a1ff2dd85972462160b7d7
Harrison Chikwe <nhcdeveloper@gmail.com>
2020-09-28 17:25:10 +0100

    Add support for Get Expressions (Property Access) on instances of classes.
    Update Parser.call to match DOT expression (operator).
    
    Add Resolver.visitGetExpr
    Add Interpreter.visitGetExpr
    Add LoxInstance.get
    Add LoxInstance.fields

--- src/main/java/com/example/lox/jlox/LoxInstance.java
+++ src/main/java/com/example/lox/jlox/LoxInstance.java
@@ -1,10 +1,22 @@
 package com.example.lox.jlox;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class LoxInstance {
     final LoxClass klass;
+    private final Map<String, Object> fields = new HashMap<>();
 
     public LoxInstance(LoxClass klass) {
         this.klass = klass;
+    }
+
+    Object get(Token name) {
+        if (fields.containsKey(name.lexeme)) {
+            return fields.get(name.lexeme);
+        }
+
+        throw new RuntimeError(name, "Undefined Property '" + name.lexeme + "'.");
     }
 
     @Override
