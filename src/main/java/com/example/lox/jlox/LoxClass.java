09a773afcddf7aeca53b41b737fc0b094b00f033
Harrison Chikwe <nhcdeveloper@gmail.com>
2020-09-28 14:54:49 +0100

    Update Resolver.java and Interpreter.java match class declaration.
    Add LoxClass.java

---
+++ src/main/java/com/example/lox/jlox/LoxClass.java
@@ -0,0 +1,14 @@
+package com.example.lox.jlox;
+
+public class LoxClass {
+    final String name;
+
+    public LoxClass(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
