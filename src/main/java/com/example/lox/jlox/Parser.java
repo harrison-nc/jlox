d8d7d3e571a8b152b301b6a722efce990b398e2d
Harrison Chikwe <nhcdeveloper@gmail.com>
2020-09-28 14:54:04 +0100

    Update Parser.java to parse Class declaration
    Add Parser.classDeclaration

--- src/main/java/com/example/lox/jlox/Parser.java
+++ src/main/java/com/example/lox/jlox/Parser.java
@@ -180,6 +180,7 @@ class Parser {
 
     private Stmt declaration() {
         try {
+            if (match(CLASS)) return classDeclaration();
             if (match(FUN)) return function("function");
             if (match(VAR)) return varDeclaration();
 
@@ -188,6 +189,19 @@ class Parser {
             synchronize();
             return null;
         }
+    }
+
+    private Stmt classDeclaration() {
+        Token name = consume(IDENTIFIER, "Expect class name.");
+        consume(LEFT_BRACE, "Expect '{' before class body.");
+
+        List<Stmt.Function> methods = new ArrayList<>();
+        while (!check(RIGHT_BRACE) && !isAtEnd()) {
+            methods.add(function("method"));
+        }
+
+        consume(RIGHT_BRACE, "Expect '}' after class body.");
+        return new Stmt.Class(name, List.copyOf(methods));
     }
 
     private Stmt.Function function(String kind) {
